95ebd17f1dc541a08d9cad3bee2b1bde81d9614a
Valkozaur <val.Nikolov03@gmail.com>
2020-02-16 22:40:58 +0100

    new commit 16/02

---
+++ Lists-Exercice/1. Train/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var train = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+            var wagonsMaxCapacity = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+
+                var splittedInput = input
+                    .Split(" ");
+
+                if (splittedInput[0] == "Add")
+                {
+                    var newWagon = int.Parse(splittedInput[1]);
+                    train.Add(newWagon);
+                }
+                else
+                {
+                    var passengersToFit = int.Parse(splittedInput[0]);
+
+                    for (int i = 0; i < train.Count; i++)
+                    {
+                        if (train[i] + passengersToFit <= wagonsMaxCapacity)
+                        {
+                            train[i] += passengersToFit;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", train));
+        }
+    }
+}
